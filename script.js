fca38545f835e19a070a918b82261926a711177e
Willie <otameganes@gmail.com>
2021-06-30 22:22:16 +0700

    Hamburger

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let navbar = document.getElementById('nav');
+const navbar = document.getElementById('nav');
+const hamburg = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 
 window.addEventListener("scroll", ()=>{
     navbar.classList.toggle('sticky', window.scrollY > 0);
 });
+
+hamburg.addEventListener('click', ()=>{
+    menu.classList.toggle('displays')
+})
