9c593665f848bc1508f69c666e52fe17b3a235b3
Willie <otameganes@gmail.com>
2021-07-03 00:40:58 +0700

    final 1.0

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navbar = document.getElementById('nav');
 const hamburg = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
+
 window.addEventListener("scroll", ()=>{
     navbar.classList.toggle('sticky', window.scrollY > 0);
 });
@@ -10,3 +11,4 @@ hamburg.addEventListener('click', ()=>{
     menu.classList.toggle('displays')
     hamburg.classList.toggle('isActive')
 })
+
